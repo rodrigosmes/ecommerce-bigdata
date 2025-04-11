679b7a0bb62a81d0bb8068424b9bd06637923289
rodrigosmes <126490594+rodrigosmes@users.noreply.github.com>
2025-04-10 21:16:00 -0300

    Create HealthCheckController.java

---
+++ mall/src/main/java/com/ibmec/mall/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.ibmec.mall.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Application is up and running!");
+    }
+
+}
